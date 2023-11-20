09f91f4d85ead7eac7ed517b64f3c96aa1ee0591
victordeluna <145511187+victordeluna@users.noreply.github.com>
2023-11-20 03:53:07 +0100

    Update login.js

--- demo/src/main/resources/static/login.js
+++ demo/src/main/resources/static/login.js
@@ -35,3 +35,18 @@ async function login(event){
     }
 }
 
+/*var username = null;
+
+window.onload=function testSecureEndpoint(){
+
+    var access_token = sessionStorage.getItem("access_token");
+
+    console.log(access_token);
+    if(access_token === null) {
+        document.location.href="/login.html";
+    }
+}
+
+function logout(){
+    sessionStorage.clear();
+}*/
