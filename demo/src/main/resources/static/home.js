5de02cb0d85a33e81d13e71f0a5a28a829fc3022
victordeluna <145511187+victordeluna@users.noreply.github.com>
2023-11-20 03:50:21 +0100

    Update home.js

--- demo/src/main/resources/static/home.js
+++ demo/src/main/resources/static/home.js
@@ -1,4 +1,17 @@
 'use strict';
+
+var username = null;
+
+window.onload=function testSecureEndpoint(){
+
+    var access_token = sessionStorage.getItem("access_token");
+
+    console.log(access_token);
+    if(access_token === null) {
+        document.location.href="/login.html";
+    }
+}
+
 function menuroommates(event){
     event.preventDefault();
     document.location.href="/roommate.html";
@@ -10,4 +23,21 @@ function menulists(event){
 function menuaccounts(event){
     event.preventDefault();
     document.location.href="/account.html";
+}
+
+var username = null;
+
+window.onload=function testSecureEndpoint(){
+
+    var access_token = sessionStorage.getItem("access_token");
+
+    console.log(access_token);
+    if(access_token === null) {
+        document.location.href="/login.html";
+    }
+}
+
+function logout(){
+    alert("You have been logged out");
+    sessionStorage.clear();
 }
