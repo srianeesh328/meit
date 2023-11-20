6d88de8ca9dbe83107d20ab5ea9de4212f9f2085
victordeluna <145511187+victordeluna@users.noreply.github.com>
2023-11-20 03:51:53 +0100

    Update index.js

--- demo/src/main/resources/static/index.js
+++ demo/src/main/resources/static/index.js
@@ -1,5 +1,8 @@
 
 'use strict';
+
+
+
 function openregister(event){
     event.preventDefault();
     document.location.href="/register.html";
@@ -10,3 +13,18 @@ function openlogin(event){
     document.location.href="/login.html";
 }
 
+/*var username = null;
+
+window.onload=function testSecureEndpoint(){
+
+    var access_token = sessionStorage.getItem("access_token");
+
+    console.log(access_token);
+    if(access_token === null) {
+        document.location.href="/login.html";
+    }
+}
+
+function logout(){
+    sessionStorage.clear();
+}*/
